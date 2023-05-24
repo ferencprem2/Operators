8b35a2522b108b6dc1087bbe6c3028e9ae6562c2
ferencprem2 <fercsusz0508@gmail.com>
2023-05-24 13:04:14 +0200

    Minden kész

--- Program.cs
+++ Program.cs
@@ -25,10 +25,25 @@ namespace Operators
             Console.WriteLine("5. feladat: Statisztika");
             Operations.Where(x => new string[6]{"mod", "/", "div", "-", "*", "+"}.Contains(x.Operand1)).GroupBy(x => x.Operand1).ToList().ForEach(x => Console.WriteLine($"\t {x.Key} -> {x.Count()}"));
 
-            //6.feladat:
+            //7.feladat:
+            do
+            {
+                Console.WriteLine($"7. feladat: Kérek egy kifejezést (pl.: 1 + 1):");
+                string userTerm = Console.ReadLine();
 
 
+                if (userTerm == "vége")
+                {
+                    break;
+                }
 
+                Console.WriteLine(OperatorHandler(userTerm));
+            } while (true);
+
+
+            //8.feladat:
+            Console.WriteLine("8.feladat: eredmenyek.txt");
+            File.WriteAllLines("eredmenyek.txt", Operations.Select(x => OperatorHandler($"{x.FirstNumber1} {x.Operand1} {x.LastNumber1}")));
 
         }
 
@@ -38,40 +53,53 @@ namespace Operators
 
         }
 
+        //6.feladat:
         static string OperatorHandler(string term)
         {
-            int firstNumber = Convert.ToInt32(term.Split("")[0]);
-            string currentOperator = term.Split("")[1];
-            int secondNumber = Convert.ToInt32(term.Split("")[2]);
-            string[] operatorArray = {"mod", "/", "div", "-", "*", "+" };
+
+        int firstNumber = Convert.ToInt32(term.Split(" ")[0]);
+        string currentOperator = term.Split(" ")[1];
+        int secondNumber = Convert.ToInt32(term.Split(" ")[2]);
+        string[] operatorArray = { "mod", "/", "div", "-", "*", "+" };
+        var value = string.Empty;
+        try
+        {
             if (operatorArray.Contains(currentOperator))
             {
-                    switch (currentOperator)
-                    {
-                        case "+":
-                            return $"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}";
-                        case "-":
-                            return $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}";
-                        case "*":
-                            return $"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}";
-                        case "/":
-                            return $"{firstNumber} / {secondNumber} = {Convert.ToDouble(firstNumber) / Convert.ToDouble(secondNumber)}";
-                        case "mod":
-                            return $"{firstNumber} div {secondNumber} = {firstNumber / secondNumber}";
-                        case "div":
-                            return $"{firstNumber} mod {secondNumber} = {firstNumber % secondNumber}";
-                    }
+                switch (currentOperator)
+                {
+                    case "+":
+                        value = $"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}";
+                        break;
+                    case "-":
+                        value = $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}";
+                        break;
+                    case "*":
+                        value = $"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}";
+                        break;
+                    case "/":
+                        value = $"{firstNumber} / {secondNumber} = {Convert.ToDouble(firstNumber) / Convert.ToDouble(secondNumber)}";
+                        break;
+                    case "mod":
+                        value =  $"{firstNumber} mod {secondNumber} = {firstNumber % secondNumber}";
+                        break;
+                    case "div":
+                        value = $"{firstNumber} div {secondNumber} = {firstNumber / secondNumber}";
+                        break;
+                }
             }
             else if (!operatorArray.Contains(currentOperator))
             {
-                return "Hibás operátor!";
+                value = "Hibás operátor!";
             }
-            else
-            {
-                return "Egyéb hiba!";
-            }
+        }
+        catch (Exception)
+        {
+            value = "Egyéb hiba!";
+        }
 
-            return "";
+        return value;
+
         }
     }
 }
