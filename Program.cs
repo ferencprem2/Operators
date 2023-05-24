2e5bc9dba2cdae5d711fbe57bf411b0154256b66
ferencprem2 <fercsusz0508@gmail.com>
2023-05-24 12:06:36 +0200

    6.feladat kész

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Operators
     internal class Program
     {
         static public List<Operands> Operations = new();
+
         static void Main(string[] args)
         {
             ParseTxtContent();
@@ -22,13 +23,55 @@ namespace Operators
 
             //5.feladat:
             Console.WriteLine("5. feladat: Statisztika");
-            Operations.GroupBy(x => x.Operand1).Select(y => new { op = y.Key, count = y.Count()}).OrderBy(x => x.op).ToList().ForEach(x => Console.WriteLine($"{x.op} -> {x.count}"));
+            Operations.Where(x => new string[6]{"mod", "/", "div", "-", "*", "+"}.Contains(x.Operand1)).GroupBy(x => x.Operand1).ToList().ForEach(x => Console.WriteLine($"\t {x.Key} -> {x.Count()}"));
+
+            //6.feladat:
+
+
+
+
         }
 
         static void ParseTxtContent()
         {
             Operations = File.ReadAllLines("kifejezesek.txt").Select(x => new Operands(x)).ToList();
 
+        }
+
+        static string OperatorHandler(string term)
+        {
+            int firstNumber = Convert.ToInt32(term.Split("")[0]);
+            string currentOperator = term.Split("")[1];
+            int secondNumber = Convert.ToInt32(term.Split("")[2]);
+            string[] operatorArray = {"mod", "/", "div", "-", "*", "+" };
+            if (operatorArray.Contains(currentOperator))
+            {
+                    switch (currentOperator)
+                    {
+                        case "+":
+                            return $"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}";
+                        case "-":
+                            return $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}";
+                        case "*":
+                            return $"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}";
+                        case "/":
+                            return $"{firstNumber} / {secondNumber} = {Convert.ToDouble(firstNumber) / Convert.ToDouble(secondNumber)}";
+                        case "mod":
+                            return $"{firstNumber} div {secondNumber} = {firstNumber / secondNumber}";
+                        case "div":
+                            return $"{firstNumber} mod {secondNumber} = {firstNumber % secondNumber}";
+                    }
+            }
+            else if (!operatorArray.Contains(currentOperator))
+            {
+                return "Hibás operátor!";
+            }
+            else
+            {
+                return "Egyéb hiba!";
+            }
+
+            return "";
         }
     }
 }
