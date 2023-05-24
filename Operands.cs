0b3246fe76f895b82e11008086e3cc36f06730d0
ferencprem2 <fercsusz0508@gmail.com>
2023-05-24 10:49:10 +0200

    4 feladat kész

---
+++ Operands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Operands
+    {
+        private int FirstNumber;
+        private string Operand;
+        private int LastNumber;
+
+        public Operands(string csvline) 
+        {
+            var lines = csvline.Split(" ");
+
+            FirstNumber = Convert.ToInt32(lines[0]);
+            Operand = lines[1]; 
+            LastNumber = Convert.ToInt32(lines[2]);
+        
+        }   
+
+        public Operands(int firstNumber, string operand, int lastNumber) 
+        {
+            this.FirstNumber = firstNumber;
+            this.Operand = operand;
+            this.LastNumber = lastNumber;
+        }
+
+        public int FirstNumber1 { get => FirstNumber; set => FirstNumber = value; }
+        public string Operand1 { get => Operand; set => Operand = value; }
+        public int LastNumber1 { get => LastNumber; set => LastNumber = value; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Operators
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Operators
 {
     internal class Program
     {
+        static public List<Operands> Operations = new();
         static void Main(string[] args)
         {
-            
+            ParseTxtContent();
+
+            //2.fealdat
+            Console.WriteLine($"2. fealdat: {Operations.Select(x => x.Operand1).Count()}");
+
+            //3.feladat:
+            Console.WriteLine($"3. feladat: {Operations.Where(x => x.Operand1 == "mod").Count()}");
+
+            //4.feladat:
+            var isInList = Operations.Any(x => x.FirstNumber1 % 10 == 0 && x.LastNumber1 % 10 == 0) ? "Van ilyen kifejezés!" : "Nincs ilyen kifejezés";
+            Console.WriteLine($"4. fealdat: {isInList}");
+
+            //5.feladat:
+            Console.WriteLine("5. feladat: Statisztika");
+            Operations.GroupBy(x => x.Operand1).Select(y => new { op = y.Key, count = y.Count()}).OrderBy(x => x.op).ToList().ForEach(x => Console.WriteLine($"{x.op} -> {x.count}"));
+        }
+
+        static void ParseTxtContent()
+        {
+            Operations = File.ReadAllLines("kifejezesek.txt").Select(x => new Operands(x)).ToList();
+
         }
     }
 }
